f44facdbaee84fd88c03853df7423fe19ff7d423
madriesen <martijn.driesen5@hotmail.com>
2021-06-10 09:49:22 +0200

    fix: add fontawesome

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import { FontAwesomeIcon } from "@/plugins/font-awesome.js";
+
+import "@/assets/css/style.css";
+
 createApp(App)
   .use(store)
   .use(router)
+  .component("fa", FontAwesomeIcon)
   .mount("#app");
